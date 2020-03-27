0094992a0701ee675f593042bc197c823f3e0326
van <van@fagaceae.com>
2020-03-27 16:16:09 +0000

    added inv.sh

---
+++ inv.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# update ubuntu to the latest version
+sudo apt update && sudo apt upgrade
+
+# setup git
+# brew install git
+git config --global user.name "van"
+git config --global user.email van@fagaceae.com
+
+
