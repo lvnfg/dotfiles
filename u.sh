584f5f62c09698eeda03dbdaee5a1af54e661fec
van <van@fagaceae.com>
2020-03-27 16:31:09 +0000

    removed code, btt.json, mac.json

--- u.sh
+++ u.sh
@@ -5,6 +5,8 @@ sudo apt update && sudo apt upgrade
 
 # setup git
 # brew install git
+git config --global credential.helper store
+git pull # run this after config to save credential
 git config --global user.name "van"
 git config --global user.email van@fagaceae.com
 sudo apt install python3-pip
