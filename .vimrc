747956f2c7b1b579b39b6838f870f9ca1276e7c6
van <van@fagaceae.com>
2020-03-31 17:56:07 +0000

    updated .vimrc, updated u.sh with odbc driver install steps

--- .vimrc
+++ .vimrc
@@ -1,1 +1,14 @@
-" comment in .vimrc start with an open quote (without a close)
+set nocompatible "disable vi compatibility and enable advanced vim features
+" set number "show line numbers
+set ignorecase "do case insensitive matching (?)
+:let loaded_matchparen = 1 "disable show matching parentheses
+syntax on "enable syntax highlighting
+
+"indenting
+set autoindent
+set copyindent
+set preserveindent
+set expandtab
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4 "when identing with >
--- u.sh
+++ u.sh
@@ -9,5 +9,29 @@ git config --global credential.helper store
 git pull # run this after config to save credential
 git config --global user.name "van"
 git config --global user.email van@fagaceae.com
-sudo apt install python3-pip
 
+# apply dotfiles settings
+git clone https://github.com/lvnfg/dot
+ln -s ~/dot/.vimrc ~/.vimrc
+
+# install odbc driver
+sudo su
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+#Ubuntu 19.10 package
+curl https://packages.microsoft.com/config/ubuntu/19.10/prod.list > /etc/apt/sources.list.d/mssql-release.list;
+
+exit
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+# optional: for bcp and sqlcmd
+sudo ACCEPT_EULA=Y apt-get install mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install unixodbc-dev
+
+# setup python
+sudo apt install python3-pip
+sudo apt install unixodbc-dev
+pip3 install --user pyodbc
