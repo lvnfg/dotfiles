2b39c01af051256449f2ebc26685edd6921f8f1a
van@vmDev <->
2020-04-11 09:51:45 +0000

    moving i.py work to numbers, will return later if free time availabel

--- i.py
+++ i.py
@@ -1,13 +1,28 @@
-ps = 19
-pf = 2
-pq = 5000
+quantity = input()
+
+putStrike = 19
+putPrice = 2
+putQuantity = 0 
+putProfit = 0
+putMargin = 0
 
 cs = 0
 cf = 0
 cq = 0
+callProfit = 0
+callMargin = 0
 
 sp = 0
 sq = 0
+stockProfit = 0
+stockMargin = 0
+
+putQuantity = q if pq == 0 else pq
+cq = q if cq == 0 else cq
+sq = q if sq == 0 else sq
+
+putFee = putPrice * putQuantity
+cft = cf * cq
 
 referencePrice = 0
 if sp > 0:
@@ -15,7 +30,7 @@ if sp > 0:
 elif cs > 0:
     referencePrice = cs
 else:
-    referencePrice = ps
+    referencePrice = putStrike
 
 step = 0
 if referencePrice < 4:
@@ -27,7 +42,21 @@ else:
 
 price = 0
 while price <= referencePrice * 1.5:
-    putProfit = (ps - price - pf) * pq if ps >= price else "--"
-    putString = str(putProfit)
-    print(" -- | " + str(price) + " | " + putString)
+    putProfit = (putStrike - price) * putQuantity - putFee
+    putMargin = ((putProfit - putFee) / putFee) * 100
+    priceVSputStrike = ((price - putStrike) / ps) * 100
+
+    callProfit = (price - cs) * cq - cft
+    callMargin = ((callProfit - cft) / cft) * 100 if cs > 0 else 0
+    priceVScs = ((price - cs) / cs) * 100 if cs > 0 else 0
+
+    stockProfit = (price - sp) * sq
+    stockmargin = (price - sp) / sp * 100
+    
+    print(
+        str(callProfit) + " " + str(callMargin) + "% " + str(priceVScs) + "%"
+        + " | " + str(price) + " | "
+        + str(priceVSputStrike) + "% " + str(putMargin) + "% " + str(putProfit)
+        + " || " + " -- " if sp == 0 else str(stockProfit) + " " + str(stockMargin) + "%"
+    )
     price += step
