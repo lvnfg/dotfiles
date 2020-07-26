8f153fd952fb9843adcea306fa25c4e210a3e396
van <->
2020-07-26 17:57:57 +0000

    cleanup follow up

--- vmdev.sh
+++ vmdev.sh
@@ -10,9 +10,7 @@ git clone git@github.com:lvnfg/dotfiles.git     ~/repos/dotfiles
 git config --global core.editor "vim"
 git config --global user.name "van"
 git config --global user.email "-"
-rm ~/.bashrc && ln -s ~/repos/dotfiles/.bashrc      ~/.bashrc       && source .bashrc
-ln -s ~/repos/dotfiles/.tmux.conf   ~/.tmux.conf
-
+rm ~/.bashrc && ln -s ~/repos/dotfiles/.bashrc ~/.bashrc && source .bashrc
 # disable ssh password login, including root
 sudo vim /etc/ssh/sshd_config
     # change the following lines to no 
@@ -22,15 +20,13 @@ sudo vim /etc/ssh/sshd_config
     #   PermitRootLogin no
 sudo systemctl restart ssh
 
-# common development tools
+# development tools
 sudo apt install postgresql-client
-
 # desktop environment, if necessary
 sudo apt install lxde
 sudo apt install xrdp
 sudo systemctl status xrdp  # verify xrdp is running
-sudo vim /etc/xrdp/startwm.sh   # add to end of file:
-                                #   lxsession -s LXDE -e LXDE
+sudo vim /etc/xrdp/startwm.sh   # add to end of file: lxsession -s LXDE -e LXDE
 sudo service xrdp restart
 sudo systemctl set-default multi-user.target    # disable GUI on boot. Use graphical.target to reenable
 sudo passwd van # set user password to connect over xrdp if none existing
