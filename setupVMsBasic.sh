c97e0a88d7f662d6ee3205748f0aae18b050924c
van <->
2020-08-11 09:56:00 +0000

    removed vim plugins from repos and created setup script instead

--- setupVMsBasic.sh
+++ setupVMsBasic.sh
@@ -11,19 +11,23 @@ git config --global credential.helper cache 3600
 git config --global core.editor "vim"
 git config --global user.name "van"
 git config --global user.email "-"
-git clone https://github.com/lvnfg/dotfiles
+# git clone https://github.com/lvnfg/dotfiles
 
 # set case-insensitive autocomplettion
 echo set completion-ignore-case on | sudo tee -a /etc/inputrc
 
 echo Creating dotfies simlinks
 ln -s ~/dotfiles/.vimrc         ~/.vimrc
-ln -s ~/dotfiles/.vim           ~/.vim
 ln -s ~/dotfiles/.tmux.conf 	~/.tmux.conf
 ln -s ~/dotfiles/.ssh/config    ~/.ssh/config
 rm ~/.bashrc
 ln -s ~/dotfiles/.bashrc		~/.bashrc
 source ~/.bashrc
+
+echo Cloning .vim plugins
+mkdir -p ~/.vim/pack/bundle/start
+cd ~/.vim/pack/bundle/start
+git clone https://github.com/airblade/vim-gitgutter
 
 echo Remember to disable all ssh password login, including root
 # sudo vim /etc/ssh/sshd_config
