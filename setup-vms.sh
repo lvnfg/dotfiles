613fa3dab1211c82e48289c5d4f9fd9ed4a37452
van <->
2020-08-18 09:38:20 +0000

    rearranged setup order

--- setup-vms.sh
+++ setup-vms.sh
@@ -2,11 +2,20 @@
 
 # common setup for all GNU debian linux VMs
 
-echo Installing tmux
+echo Installing apps
 sudo apt install -y tmux
-
-echo Installing git
+sudo apt install -y fzf
 sudo apt install -y git
+
+echo Creating symlinks
+ln -s ~/dotfiles/.vimrc         ~/.vimrc
+ln -s ~/dotfiles/.tmux.conf 	~/.tmux.conf
+ln -s ~/dotfiles/.ssh/config    ~/.ssh/config
+ln -s ~/dotfiles/.inputrc       ~/.inputrc
+rm ~/.bashrc
+ln -s ~/dotfiles/.bashrc		~/.bashrc
+
+echo Configuring git
 git config --global credential.helper cache 3600
 git config --global core.editor "vim"
 git config --global user.name "van"
@@ -15,19 +24,9 @@ git config --global user.email "-"
 # set case-insensitive autocomplettion
 echo set completion-ignore-case on | sudo tee -a /etc/inputrc
 
-echo Creating dotfies simlinks
-ln -s ~/dotfiles/.vimrc         ~/.vimrc
-ln -s ~/dotfiles/.tmux.conf 	~/.tmux.conf
-ln -s ~/dotfiles/.ssh/config    ~/.ssh/config
-ln -s ~/dotfiles/.inputrc       ~/.inputrc
-rm ~/.bashrc
-ln -s ~/dotfiles/.bashrc		~/.bashrc
-
-echo Purging pre-installed VIM
-sudo apt purge -y vim
-sudo apt autoremove
+echo Setting up VIM
 echo Installing latest VIM from source
-git clone git@github.com:vim/vim.git ~/vim
+git clone git@github.com:vim/vim ~/vim
 cd ~/vim/src
 sudo apt install -y libncurses5-dev
 make distclean  # clear previous install if any
@@ -36,16 +35,15 @@ sudo make install
 sudo mv ~/vim/src/vim /usr/bin
 echo Clearning up
 sudo rm -rf ~/vim
-echo VIM installation done
-
+sudo apt purge -y vim
+sudo apt autoremove -y
+which vim
+vim --version
 echo Cloning .vim plugins
 mkdir -p ~/.vim/pack/bundle/start
 cd ~/.vim/pack/bundle/start
 git clone git@github.com:airblade/vim-gitgutter
 git clone git@github.com:itchyny/lightline.vim
-
-echo Installing fzf
-sudo apt install -y fzf
 
 echo Remember to disable all ssh password login, including root
 # sudo vim /etc/ssh/sshd_config
