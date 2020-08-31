3aa2b9a65a6703b686e3514da38d2eeba3a3c26c
van <->
2020-08-31 18:23:46 +0000

    fixed ~/.ssh/config permission on pc

--- setup.sh
+++ setup.sh
@@ -25,8 +25,9 @@ git config --global user.email "-"
 rm      	~/.bashrc
 ln -s ~/dotfiles/.bashrc		~/.bashrc
 ln -s ~/dotfiles/.inputrc		~/.inputrc
-ln -s ~/dotfiles/.ssh/config	~/.ssh/config
 ln -s ~/dotfiles/.tmux.conf		~/.tmux.conf
+ln -s ~/dotfiles/.ssh/config	~/.ssh/config
+sudo chmod 600 ~/.ssh/config
 
 # Neovim
 # ----------------------------------------------------------------
