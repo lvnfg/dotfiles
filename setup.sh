637a4ecf9ec09a56ffc42323cc37856770292310
van <->
2020-09-01 14:58:44 +0700

    fixed setup for origin and made all features except language server available for wsl hosts

--- setup.sh
+++ setup.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+# Disable all ssh password login, including root
+# sudo vim /etc/ssh/sshd_config
+# change the following lines to no 
+#   ChallengeResponseAuthentication no
+#   PasswordAuthentication no
+#   UsePAM no
+#   PermitRootLogin no
+# sudo systemctl restart ssh
+
+# ssh-keygen -t ed25519 -C "van@$HOSTNAME"
+
+hostType=vm
+if [[ "$HOSTNAME" = DESKTOP ]] || [[ "$HOSTNAME" = origin ]]; then
+	hostType=wsl	
+fi
+
 # Basic utilities
 # ----------------------------------------------------------------
-if [[ "$HOSTNAME" = DESKTOP ]] || [[ "$HOSTNAME" = ORIGIN ]]; then
-	sudo apt update && sudo apt upgrade -y
-	sudo apt install -y openssh-client
-	sudo apt install -y git
-else
-	sudo apt install -y wget
-	sudo apt install -y unzip
-	sudo apt install -y git
-	sudo apt install -y python3-pip
-fi
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y openssh-client
 sudo apt install -y tmux
 sudo apt install -y fzf
+sudo apt install -y git
+sudo apt install -y wget
+sudo apt install -y unzip
+sudo apt install -y curl
+sudo apt install -y python3-pip
 
 # Dotfiles and environment setup
 # ----------------------------------------------------------------
@@ -31,24 +44,20 @@ sudo chmod 600 ~/.ssh/config
 
 # Neovim
 # ----------------------------------------------------------------
-if [[ "$HOSTNAME" = DESKTOP ]] || [[ "$HOSTNAME" = ORIGIN ]]; then
-	sudo apt install -y neovim
-else
-	sudo apt install -y fuse
-	wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-	chmod u+x nvim.appimage
-	sudo mv nvim.appimage /usr/bin/
-	sudo apt purge -y vim
-	sudo apt autoremove -y
-	echo Setting nvim as default and alternatives
-	nvimPath="/usr/bin/nvim.appimage"
-	set -u
-	sudo update-alternatives --install /usr/bin/ex		ex			"$nvimPath" 110
-	sudo update-alternatives --install /usr/bin/vi		vi			"$nvimPath" 110
-	sudo update-alternatives --install /usr/bin/view	view		"$nvimPath" 110
-	sudo update-alternatives --install /usr/bin/vim		vim			"$nvimPath" 110
-	sudo update-alternatives --install /usr/bin/vimdiff vim diff	"$nvimPath" 110
-fi	
+sudo apt install -y fuse
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/bin/
+sudo apt purge -y vim
+sudo apt autoremove -y
+echo Setting nvim as default and alternatives
+nvimPath="/usr/bin/nvim.appimage"
+set -u
+sudo update-alternatives --install /usr/bin/ex		ex			"$nvimPath" 110
+sudo update-alternatives --install /usr/bin/vi		vi			"$nvimPath" 110
+sudo update-alternatives --install /usr/bin/view	view		"$nvimPath" 110
+sudo update-alternatives --install /usr/bin/vim		vim			"$nvimPath" 110
+sudo update-alternatives --install /usr/bin/vimdiff vim diff	"$nvimPath" 110
 echo Installing plug.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -57,11 +66,7 @@ mkdir -p	~/.config/nvim/lua
 touch		~/.config/nvim/lua/init.lua
 ln -s ~/dotfiles/init.vim		~/.config/nvim/
 ln -s ~/dotfiles/.vimrc			~/.vimrc
-if [[ "$HOSTNAME" = DESKTOP ]] || [[ "$HOSTNAME" = ORIGIN ]]; then
-	nvim --headless -c 'PlugInstall' +qall
-else
-	nvim.appimage --headless -c 'PlugInstall' +qall
-fi
+nvim.appimage --headless -c 'PlugInstall' +qall
 echo Creating init.lua symlink
 rm								~/.config/nvim/lua/init.lua
 ln -s ~/dotfiles/init.lua		~/.config/nvim/lua/
@@ -69,8 +74,9 @@ ln -s ~/dotfiles/init.lua		~/.config/nvim/lua/
 
 # Language servers
 # ----------------------------------------------------------------
-if [[ "$HOSTNAME" = DESKTOP ]] || [[ "$HOSTNAME" = ORIGIN ]]; then
+if [[ $hostType = wsl ]]; then
 	# Skip
+	echo Language server setup skipped for $hostType
 else
 	# Microsoft python language server
 	echo Installing .Net core SDK
@@ -88,16 +94,5 @@ else
 	pip3 install pynvim
 fi
 
-echo Remember to disable all ssh password login, including root
-# sudo vim /etc/ssh/sshd_config
-# change the following lines to no 
-#   ChallengeResponseAuthentication no
-#   PasswordAuthentication no
-#   UsePAM no
-#   PermitRootLogin no
-# sudo systemctl restart ssh
-
-echo Remember to generate ssh key if needed
-# ssh-keygen -t rsa -b 4096 -C "van@$HOSTNAME"
 
 echo 'Done. Remember to source .bashrc, exec bash -l, and gcloud init (if this is the first time run)'
