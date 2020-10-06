6871c124d5fa5d2723ca9b2c332c7ffa4e4f95e6
van <->
2020-10-06 05:52:32 +0000

    fix wsl & macbook setup.sh do nothing

--- setup.sh
+++ setup.sh
@@ -21,51 +21,52 @@ fi
 if [[ $hostType = macbook ]] || [[ $hostType = wsl ]]; then
     ln -s ~/dotfiles/.ssh/config		~/.ssh/config
     sudo chmod 600 ~/.ssh/config
-else
-    # Basic utilities
-    # ----------------------------------------------------------------
-    sudo apt update && sudo apt upgrade -y
-    sudo apt install -y openssh-client
-    sudo apt install -y tmux
-    sudo apt install -y fzf
-    sudo apt install -y git
-    sudo apt install -y wget
-    sudo apt install -y unzip
-    sudo apt install -y curl
-    sudo apt install -y python3-pip
-
-    # Dotfiles and environment setup
-    # ----------------------------------------------------------------
-    ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-    git clone git@github.com:lvnfg/dotfiles		~/dotfiles
-    git config --global core.editor "vim"
-    git config --global user.name "van"
-    git config --global user.email "-"
-    rm      	~/.bashrc
-    ln -s ~/dotfiles/.bashrc		~/.bashrc
-    ln -s ~/dotfiles/.inputrc		~/.inputrc
-    ln -s ~/dotfiles/.tmux.conf		~/.tmux.conf
-    ln -s ~/dotfiles/.vimrc			~/.vimrc
-
-	# Neovim
-	# ----------------------------------------------------------------
-	sudo apt install -y fuse
-	wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-	chmod u+x nvim.appimage
-	sudo mv nvim.appimage /usr/bin/
-	echo Setting nvim as default and alternatives
-	nvimPath="/usr/bin/nvim.appimage"
-	set -u
-	sudo update-alternatives --install /usr/bin/ex		ex			"$nvimPath" 110
-	sudo update-alternatives --install /usr/bin/vi		vi			"$nvimPath" 110
-	sudo update-alternatives --install /usr/bin/view	view		"$nvimPath" 110
-	sudo update-alternatives --install /usr/bin/vim		vim			"$nvimPath" 110
-	sudo update-alternatives --install /usr/bin/vimdiff vim diff	"$nvimPath" 110
-	ln -s ~/dotfiles/.vimrc 		~/.config/nvim/
-	echo Installing plug.vim
-	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-		   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-	nvim.appimage --headless -c 'PlugInstall' +qall
-	ln -s ~/dotfiles/init.lua		~/.config/nvim/lua/
 fi
+    
+# Basic utilities
+# ----------------------------------------------------------------
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y openssh-client
+sudo apt install -y tmux
+sudo apt install -y fzf
+sudo apt install -y git
+sudo apt install -y wget
+sudo apt install -y unzip
+sudo apt install -y curl
+sudo apt install -y python3-pip
+
+# Dotfiles and environment setup
+# ----------------------------------------------------------------
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+git clone git@github.com:lvnfg/dotfiles		~/dotfiles
+git config --global core.editor "vim"
+git config --global user.name "van"
+git config --global user.email "-"
+rm      	~/.bashrc
+ln -s ~/dotfiles/.bashrc		~/.bashrc
+ln -s ~/dotfiles/.inputrc		~/.inputrc
+ln -s ~/dotfiles/.tmux.conf		~/.tmux.conf
+ln -s ~/dotfiles/.vimrc			~/.vimrc
+
+# Neovim
+# ----------------------------------------------------------------
+sudo apt install -y fuse
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/bin/
+echo Setting nvim as default and alternatives
+nvimPath="/usr/bin/nvim.appimage"
+set -u
+sudo update-alternatives --install /usr/bin/ex		ex			"$nvimPath" 110
+sudo update-alternatives --install /usr/bin/vi		vi			"$nvimPath" 110
+sudo update-alternatives --install /usr/bin/view	view		"$nvimPath" 110
+sudo update-alternatives --install /usr/bin/vim		vim			"$nvimPath" 110
+sudo update-alternatives --install /usr/bin/vimdiff vim diff	"$nvimPath" 110
+ln -s ~/dotfiles/.vimrc 		~/.config/nvim/
+echo Installing plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim.appimage --headless -c 'PlugInstall' +qall
+ln -s ~/dotfiles/init.lua		~/.config/nvim/lua/
+
 echo 'Done. Remember to source .bashrc, exec bash -l, and gcloud init (if this is the first time run)'
