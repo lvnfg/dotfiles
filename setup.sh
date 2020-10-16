295bbf71662640cbefb4999f64ebfd909f082867
van <->
2020-10-16 08:21:05 +0000

    add docker installation

--- setup.sh
+++ setup.sh
@@ -77,4 +77,25 @@ if [[ ${task[*]} =~ nvim ]]; then
     ln -s ~/repos/dotfiles/init.lua		~/.config/nvim/lua/
 fi
 
+# Docker
+# ----------------------------------------------------------------
+if [[ ${task[*]} =~ docker ]]; then
+    sudo apt-get update
+    sudo apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg-agent \
+        software-properties-common
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    sudo apt-key fingerprint 0EBFCD88   # Verify key downloaded with correct fingerprint
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/debian \
+       $(lsb_release -cs) \
+       stable"
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+    sudo docker run hello-world         # Verify installation successful
+fi
+
 echo 'Done. Remember to source .bashrc, exec bash -l, and gcloud init (if this is the first time run)'
