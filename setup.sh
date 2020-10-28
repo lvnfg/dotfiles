b2f9cb560cca8e605a75d5533a2d225318873073
van <->
2020-10-28 15:40:53 +0000

    remove dev tools setup

--- setup.sh
+++ setup.sh
@@ -64,24 +64,6 @@ function installDocker() {
         docker run hello-world         # Verify installation successful
 }
 
-function installDevTools() {
-    sudo apt install -y python3-pip
-	# Microsoft python language server
-	echo Installing .Net core SDK
-	url="https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb"
-	fileName="dotnet.deb"
-	wget -O $fileName $url
-	dpkg -i $fileName
-	sudo apt-get update
-	sudo apt-get install -y apt-transport-https
-	sudo apt-get update
-	sudo apt-get install -y dotnet-sdk-3.1
-	rm $fileName
-	echo Add python interpreter to nvim
-	nvim.appimage --headless -c 'LspInstall pyls_ms' +qall
-	pip3 install pynvim
-}
-
 # --------------------------------
 # Actual setup scripts
 # --------------------------------
