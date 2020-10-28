114518434e2a4b4d829d06c2773d99420b1a5386
van <->
2020-10-28 14:52:04 +0000

    remove neovim from setup

--- setup.sh
+++ setup.sh
@@ -64,31 +64,6 @@ function installDocker() {
         docker run hello-world         # Verify installation successful
 }
 
-function installNeovim() {
-    mkdir -p ~/apps/nvim 
-    cd ~/apps/nvim
-    wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-    chmod u+x nvim.appimage
-    ./nvim.appimage --appimage-extract
-    sudo ln -s ~/apps/nvim/squashfs-root/usr/bin/nvim /usr/bin/nvim
-    nvimPath="/usr/bin/nvim"
-    set -u
-    sudo update-alternatives --install /usr/bin/ex		ex		    "$nvimPath" 110
-    sudo update-alternatives --install /usr/bin/vi		vi		    "$nvimPath" 110
-    sudo update-alternatives --install /usr/bin/view	    view	    "$nvimPath" 110
-    sudo update-alternatives --install /usr/bin/vim		vim		    "$nvimPath" 110
-    sudo update-alternatives --install /usr/bin/vimdiff 	vim diff	"$nvimPath" 110
-    mkdir -p ~/.config/nvim/lua/
-    rm ~/.config/nvim/init.vim 
-    rm ~/.config/nvim/lua/init.lua
-    ln -s ~/repos/dotfiles/init.vim 	~/.config/nvim/init.vim
-    echo Installing plug.vim
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    nvim.appimage --headless -c 'PlugInstall' +qall
-    ln -s ~/repos/dotfiles/init.lua	~/.config/nvim/lua/init.lua
-}
-
 function installDevTools() {
     sudo apt install -y python3-pip
 	# Microsoft python language server
@@ -125,7 +100,4 @@ if [[ ${task[0]} =~ vm ]]; then
 fi
 if [[ ${task[*]} =~ docker ]]; then
     installDocker
-fi
-if [[ ${task[*]} =~ vim ]]; then
-    installNeovim
 fi
