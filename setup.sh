52a5ab6d67dc915295e4ae0554e87ab18bed3771
van <->
2020-11-05 14:13:31 +0700

    set default git log format

--- setup.sh
+++ setup.sh
@@ -35,6 +35,11 @@ function configureGit() {
     git config --global core.editor     "vim"
     git config --global user.name       "van"
     git config --global user.email      "-"
+    git config --global format.graph
+    gitFormatFull="%C(auto)%h %d%Creset %s - %Cgreen%ad%Creset %aN <%aE>"
+    gitFormatShort="%C(auto)%h %d%Creset %s>"
+    gitFormatString="$gitFormatShort"
+    git config --global format.pretty format:"$gitFormatString"
 }
 
 function linkDotfiles() {
