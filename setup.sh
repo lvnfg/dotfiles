dae17dff51e74726924bd1743ef02fbf21015e84
van <->
2021-04-08 08:57:18 +0000

    add python-pip install

--- setup.sh
+++ setup.sh
@@ -128,6 +128,7 @@ function setupVM() {
     linkDotfiles
 	sudo apt update 
 	sudo apt upgrade -y
+	sudo apt install -y python3-pip
 	sudo apt install -y jq
 	sudo apt install -y vim
 	sudo apt install -y tmux
