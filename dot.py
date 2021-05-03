cf9364735c8709580c4042179115db0729fbb68e
van <->
2021-05-04 00:27:15 +0700

    add suffix handling to dot.py task

--- dot.py
+++ dot.py
@@ -7,8 +7,10 @@ from dataclasses import dataclass
 
 parser = argparse.ArgumentParser(description = 'Provide core functionlities to aid administrative tasks',)
 parser.add_argument('task', type=str, help='The task to be done, e.g. close, open, start, stop...')
-parser.add_argument('resource', type=str, help='The target resource of the task, e.g. vmdev, sqldev')
-parser.add_argument('-i', '--ip-address', dest='ipAddress', type=str, help='Specify the ip address to use (default: current public ip)')
+parser.add_argument('prefix', type=str, help='Provide just the prefix of the resouce group to open access to all resources within that group at once')
+parser.add_argument('-r', '--resource-group', dest='resourceGroup', type=str, help='The resource group of the resource.')
+parser.add_argument('-s', '--suffix', type=str, default='sea', help='The suffix (region code) of the resource.')
+parser.add_argument('-i', '--ip-address', dest='ipAddress', type=str, help='Specify the ip address to use (default: current public ip).')
 args = parser.parse_args()
 
 def getPublicIP():
@@ -17,39 +19,48 @@ def getPublicIP():
     print(ip)
     return ip 
 
-class NSG():
-    def __init__(self, prefix: str, name: str = None, resourceGroup: str = None):
-        self.prefix:        str = prefix
-        self.nsgName:       str = f'{prefix}-nsg-sea' if not name else name
-        self.resourceGroup: str = f'{prefix}-rg' if not resourceGroup else resourceGroup
+def updateNSG(
+    task:          str,
+    name:          str = None,
+    resourceGroup: str = None,  # Required if exact name is provided. Constructed from prefix otherwise
+    prefix:        str = None,
+    suffix:        str = None,
+    sourceIP:      str = None,
+    destinationIP: str = '*',
+    ruleName:      str = 'van',
+) -> None:
+    if not name:
+        name          = f'{prefix}-nsg-{suffix}'
+        resourceGroup = f'{prefix}-rg'
+    if not sourceIP:
+        if task == 'close':
+            sourceIP = '*'
+        else:
+            sourceIP = getPublicIP()
+    accessDict = {'open': 'allow', 'close': 'deny'}
+    accessType = accessDict[args.task]
+    
+    info = f'{accessType.upper()}: {sourceIP} -> {destinationIP} [{name}]'
+    print(f'updating: {info}')
+    command = f"""
+        az network nsg rule update  \
+        --resource-group {resourceGroup}  \
+        --nsg-name {name}  \
+        --name {ruleName}  \
+        --access {accessType}  \
+        --source-address-prefixes "{sourceIP}"  \
+        --destination-address-prefix "{destinationIP}"
+    """
+    os.system(command)
 
-    def update(self, task: str, sourceIP: str, destinationIP: str = '*', ruleName: str = 'van'):
-        if not sourceIP:
-            if args.task == 'close':
-                sourceIP = '*'
-            else:
-                sourceIP = getPublicIP()
-        
-        accessDict = {'open': 'allow', 'close': 'deny'}
-        accessType = accessDict[args.task]
-        
-        ruleName = 'van'
-        info = f'{accessType} access from {sourceIP} to {destinationIP} for {self.nsgName}'
-        print(f'updating: {info}')
-        command = f"""
-            az network nsg rule update  \
-            --resource-group {self.resourceGroup}  \
-            --nsg-name {self.nsgName}  \
-            --name {ruleName}  \
-            --access {accessType}  \
-            --source-address-prefixes "{sourceIP}"  \
-            --destination-address-prefix "{destinationIP}"
-        """
-        os.system(command)
 
 def main():
-    NSG(prefix=args.resource).update(task=args.task, sourceIP=args.ipAddress)
-
+    task = args.task
+    if task in ('open', 'close'):
+        sourceIP = '*' if args.ipAddress in ('a', 'all') else args.ipAddress
+        updateNSG(task=args.task, prefix=args.prefix, suffix=args.suffix, sourceIP=sourceIP)
+    elif task in ('start', 'stop'):
+        pass
 
 if __name__ == "__main__":
     main()
