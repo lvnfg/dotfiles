633f8c3cec914fde8e70570a4bb00d4c3da484b9
van <->
2021-05-03 23:46:57 +0700

    refactor dot.py for simplicity

--- dot.py
+++ dot.py
@@ -9,7 +9,6 @@ parser = argparse.ArgumentParser(description = 'Provide core functionlities to a
 parser.add_argument('task', type=str, help='The task to be done, e.g. close, open, start, stop...')
 parser.add_argument('resource', type=str, help='The target resource of the task, e.g. vmdev, sqldev')
 parser.add_argument('-i', '--ip-address', dest='ipAddress', type=str, help='Specify the ip address to use (default: current public ip)')
-parser.add_argument('-I', '--ip-address-all', dest='ipAddressAll', action='store_const', const=True, default=False, help='use the widest IP range possible (normally *)')
 args = parser.parse_args()
 
 def getPublicIP():
@@ -18,48 +17,23 @@ def getPublicIP():
     print(ip)
     return ip 
 
-@dataclass
-class Resource():
-    name:          str
-    resourceType:  str
-    resourceGroup: str
-    nsgName:       str = ''
-    nsgRuleName:   str = 'van'
-    
-    def parse(self):
-        if args.task in ['start', 'stop']:
-            self.powerVM()
-        elif args.task in ['open', 'close']:
-            self.updateNSG()
-   
-    def powerVM(self):
-        powerTask = None
-        if args.task == 'stop':
-            powerTask = 'deallocate'
-        else:
-            powerTask = args.task
-        print(f'preparing to {powerTask} {self.vmName}')
-        command = f"""
-            az vm {powerTask} \
-            --resource-group {self.resourceGroup} \
-            --name {self.vmName}
-        """
-        os.system(command)
+class NSG():
+    def __init__(self, prefix: str, name: str = None, resourceGroup: str = None):
+        self.prefix:        str = prefix
+        self.nsgName:       str = f'{prefix}-nsg-sea' if not name else name
+        self.resourceGroup: str = f'{prefix}-rg' if not resourceGroup else resourceGroup
 
-    def updateNSG(self):
-        sourceIP = None
-        if args.ipAddressAll == True or args.task == 'close':
-            sourceIP = '*'
-        elif args.ipAddress != None:
-            sourceIP = args.ipAddress
-        else:
-            sourceIP = getPublicIP()
+    def update(self, task: str, sourceIP: str, destinationIP: str = '*', ruleName: str = 'van'):
+        if not sourceIP:
+            if args.task == 'close':
+                sourceIP = '*'
+            else:
+                sourceIP = getPublicIP()
         
         accessDict = {'open': 'allow', 'close': 'deny'}
         accessType = accessDict[args.task]
         
         ruleName = 'van'
-        destinationIP = '*'
         info = f'{accessType} access from {sourceIP} to {destinationIP} for {self.nsgName}'
         print(f'updating: {info}')
         command = f"""
@@ -72,22 +46,10 @@ class Resource():
             --destination-address-prefix "{destinationIP}"
         """
         os.system(command)
-    
-resourceList = [
-    Resource('vmdev',    'vm', 'rgDev',         'vmdev-nsg',                     'van'),
-    Resource('vmwin',    'vm', 'rgIntegration', 'vmwin-nsg',                     'van'),
-    Resource('opsbuild', 'vm', 'opsbuild',      'opsbuild-networksecuritygroup', 'van'),
-    Resource('opsdev01', 'vm', 'opsdev01',      'opsdev01-networksecuritygroup', 'van'),
-    Resource('opsdev02', 'vm', 'opsdev02',      'opsdev02-networksecuritygroup', 'van'),
-]
 
 def main():
-    res = args.resource.lower().replace(' ', '')
-    for r in resourceList:
-        if res == 'all' or res == r.name:
-            r.parse()
-            if res != 'all':
-                return 
+    NSG(prefix=args.resource).update(task=args.task, sourceIP=args.ipAddress)
+
 
 if __name__ == "__main__":
     main()
