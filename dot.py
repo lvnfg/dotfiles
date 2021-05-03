84bade9e52f1e219c35e977a74bc34ced59e2d66
van <->
2021-05-04 00:44:39 +0700

    refactor optional suffix to positional option region code

--- dot.py
+++ dot.py
@@ -8,8 +8,7 @@ from dataclasses import dataclass
 parser = argparse.ArgumentParser(description = 'Provide core functionlities to aid administrative tasks',)
 parser.add_argument('task', type=str, help='The task to be done, e.g. close, open, start, stop...')
 parser.add_argument('prefix', type=str, help='Provide just the prefix of the resouce group to open access to all resources within that group at once')
-parser.add_argument('-r', '--resource-group', dest='resourceGroup', type=str, help='The resource group of the resource.')
-parser.add_argument('-s', '--suffix', type=str, default='sea', help='The suffix (region code) of the resource.')
+parser.add_argument('region', nargs='?', type=str, default='sea', help='Option region code for the resource, e.g. sea, usw, usn...')
 parser.add_argument('-i', '--ip-address', dest='ipAddress', type=str, help='Specify the ip address to use (default: current public ip).')
 args = parser.parse_args()
 
@@ -24,13 +23,13 @@ def updateNSG(
     name:          str = None,
     resourceGroup: str = None,  # Required if exact name is provided. Constructed from prefix otherwise
     prefix:        str = None,
-    suffix:        str = None,
+    region:        str = None,
     sourceIP:      str = None,
     destinationIP: str = '*',
     ruleName:      str = 'van',
 ) -> None:
     if not name:
-        name          = f'{prefix}-nsg-{suffix}'
+        name          = f'{prefix}-nsg-{region}'
         resourceGroup = f'{prefix}-rg'
     if not sourceIP:
         if task == 'close':
@@ -40,7 +39,7 @@ def updateNSG(
     accessDict = {'open': 'allow', 'close': 'deny'}
     accessType = accessDict[args.task]
     
-    info = f'{accessType.upper()}: {sourceIP} -> {destinationIP} [{name}]'
+    info = f'[{name}] {sourceIP} -> {destinationIP} {accessType.upper()}'
     print(f'updating: {info}')
     command = f"""
         az network nsg rule update  \
@@ -58,7 +57,7 @@ def main():
     task = args.task
     if task in ('open', 'close'):
         sourceIP = '*' if args.ipAddress in ('a', 'all') else args.ipAddress
-        updateNSG(task=args.task, prefix=args.prefix, suffix=args.suffix, sourceIP=sourceIP)
+        updateNSG(task=args.task, prefix=args.prefix, region=args.region, sourceIP=sourceIP)
     elif task in ('start', 'stop'):
         pass
 
