3c0ac32470278a044c64de610f0146eae657a156
van <->
2021-07-18 11:05:02 +0000

    add mssql-cli note

--- setup.sh
+++ setup.sh
@@ -292,6 +292,8 @@ function setup-azure-function-core-tools() {
 }
 
 function setup-mssql-cli() {
+    # The official doc is outdated and installing with apt doesn't work
+    # https://docs.microsoft.com/en-us/sql/tools/mssql-cli?view=sql-server-ver15
     pip3 install mssql-cli
     sudo ln -s -f ~/.local/bin/mssql-cli  /usr/local/bin/mssql-cli
 }
