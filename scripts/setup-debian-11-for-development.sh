4226e2ad55ce79fe80112a49512cf8a8096aa000
van <->
2021-08-20 15:32:24 +0000

    prepare setup scripts for debian 11

--- scripts/setup-debian-11-for-development.sh
+++ scripts/setup-debian-11-for-development.sh
@@ -15,6 +15,9 @@ sudo apt install -y unzip
 sudo apt install -y curl
 sudo apt install -y bash-completion
 sudo apt install -y tree
+sudo apt install -y fzf
+sudo apt install -y ranger
+sudo apt install -y bat
 
 # Symlink dotfiles
 ln -s -f $DOTFILES/.bashrc	    ~/.bashrc
@@ -23,16 +26,20 @@ ln -s -f $DOTFILES/.inputrc	    ~/.inputrc
 ln -s -f $DOTFILES/.tmux.conf   ~/.tmux.conf
 ln -s -f $DOTFILES/.vimrc	    ~/.vimrc
 
+# Install neovim
+source "$path/install-neovim.sh"
+
+# Install ranger
+sudp apt install ranger
 # Symlink ranger config
 mkdir ~/.config/ranger
 ln -s -f $DOTFILES/rc.conf	    ~/.config/ranger/rc.conf
 ln -s -f $DOTFILES/scope.sh	    ~/.config/ranger/scope.sh
-
 # Make scope.sh executable for ranger
 chmod $DOTFILES/scope.sh
 
 # Install python
-source "$path/install-python.sh"
+# source "$path/install-python.sh"
 # Install ipython
 # To enable vim mode, create profile with
 #   ipython profile create
@@ -46,21 +53,20 @@ source "$path/install-python.sh"
 #    import jedi.api.helpers
 #    import jedi.api.classes
 #    JEDI_INSTALLED = True
-pip3 install ipython
+# pip3 install ipython
 # Install other pip packages
-pip3 install mssql-cli
+# pip3 install mssql-cli
 
 # Install apps not available from apt or pip
 # Use source to use the same bash process and share variables
-source "$path/install-git.sh"
-source "$path/install-fzf.sh"
-source "$path/install-bat.sh"       # Switch to using apt when available for Debian
-source "$path/install-neovim.sh"
-source "$path/install-nodejs.sh"
-source "$path/install-ranger.sh"
-source "$path/install-netcore.sh"
-source "$path/install-azure-cli.sh"
-source "$path/install-azure-functions.sh"
+# source "$path/install-git.sh"
+# source "$path/install-fzf.sh"
+# source "$path/install-bat.sh"       # Switch to using apt when available for Debian
+# source "$path/install-nodejs.sh"
+# source "$path/install-ranger.sh"
+# source "$path/install-netcore.sh"
+# source "$path/install-azure-cli.sh"
+# source "$path/install-azure-functions.sh"
 
 
 # Allow calling functions by name from command line
