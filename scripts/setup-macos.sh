18133b646a9986d737f9dcf9a9fcada1bbf48410
van <->
2021-11-20 14:11:58 +0000

    fix macos setup

--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -3,16 +3,23 @@
 script_name=$0
 path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Source .bashrc to get core directories' location
-source ~/repos/dotfiles/.bashrc
-
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Stup git
+git config --global core.editor     "code"
+git config --global user.name       "van"
+git config --global user.email      "-"
+
 # Install neovim
-bash "$path/install-neovim.sh"
+brew install neovim
 
-# Install & setup git
-bash "$path/install-git.sh"
+# install vim-plug
+echo "installing vim plug"
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
+# symlink nvim config
+echo "creating neovim config symlinks"
+wdir="$HOME/.config/nvim" && mkdir -pv $wdir
+ln -s -f $DOTFILES/init.vim $wdir/init.vim
