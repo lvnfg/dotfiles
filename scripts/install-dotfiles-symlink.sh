7d8dc58ef9984084925ce4190b6760743c776e3c
van <->
2021-11-23 15:42:02 +0000

    add ipython_config.py

--- scripts/install-dotfiles-symlink.sh
+++ scripts/install-dotfiles-symlink.sh
@@ -1,7 +1,8 @@
 # Symlink dotfiles
-ln -s -f $DOTFILES/.bashrc        ~/.bashrc
-ln -s -f $DOTFILES/.bashrc        ~/.profile
-ln -s -f $DOTFILES/.inputrc       ~/.inputrc
-ln -s -f $DOTFILES/.tmux.conf     ~/.tmux.conf
-ln -s -f $DOTFILES/.vimrc         ~/.vimrc
-ln -s -f $DOTFILES/litecli/config ~/.config/litecli/config
+ln -s -f $DOTFILES/.bashrc           ~/.bashrc
+ln -s -f $DOTFILES/.bashrc           ~/.profile
+ln -s -f $DOTFILES/.inputrc          ~/.inputrc
+ln -s -f $DOTFILES/.tmux.conf        ~/.tmux.conf
+ln -s -f $DOTFILES/.vimrc            ~/.vimrc
+ln -s -f $DOTFILES/litecli/config    ~/.config/litecli/config
+ln -s -f $DOTFILES/ipython_config.py ~/.ipython/profile_default/ipython_config.py
