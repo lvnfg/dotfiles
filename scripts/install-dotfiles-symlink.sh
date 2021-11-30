f477e8727a64dfaa52011728f59599ee93b3657a
van <->
2021-11-30 06:00:48 +0000

    unlink bashrc from .profile (to use for env vars) + add .bash_profile

--- scripts/install-dotfiles-symlink.sh
+++ scripts/install-dotfiles-symlink.sh
@@ -1,6 +1,7 @@
 # Symlink dotfiles
+ln -s -f $DOTFILES/.bash_profile     ~/.bash_profile
 ln -s -f $DOTFILES/.bashrc           ~/.bashrc
-ln -s -f $DOTFILES/.bashrc           ~/.profile
+# ln -s -f $DOTFILES/.bashrc           ~/.profile       # .profile is reserved for env var, do not use
 ln -s -f $DOTFILES/.inputrc          ~/.inputrc
 ln -s -f $DOTFILES/.tmux.conf        ~/.tmux.conf
 ln -s -f $DOTFILES/.vimrc            ~/.vimrc
