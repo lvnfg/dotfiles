193ddd8ccc80792d6ea952049ef3ac388568a654
van <->
2021-12-28 08:18:50 +0000

    move azure tools install out of debian setup

--- scripts/setup-debian-11-for-development.sh
+++ scripts/setup-debian-11-for-development.sh
@@ -61,17 +61,5 @@ sudo ln -s -f /usr/local/bin/python3 /usr/bin/python
 # Pynvim for nvim's python plugins
 # pip3 install pynvim
 
-# ----------------------------------------------
-# AZURE TOOLS AND SERVICES
-# ----------------------------------------------
-# Install Azure tools and services
-# bash "$path/install-netcore.sh"
-# sudo apt install azure-cli
-# Debian 11 required this specific keyvault package from pip
-# and cannot use the version included in the azure-cli apt
-# pip3 install azure-keyvault==1.1.0
-# bash "$path/install-azure-functions.sh"
-
-
 # Allow calling functions by name from command line
 "$@"
