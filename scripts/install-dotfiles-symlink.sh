12f9f3664fed97af8031c7f31d0276ccedbda436
van <->
2021-12-28 07:33:25 +0000

    remove ipython dotfile from symlink script

--- scripts/install-dotfiles-symlink.sh
+++ scripts/install-dotfiles-symlink.sh
@@ -6,4 +6,3 @@ ln -s -f $DOTFILES/.inputrc          ~/.inputrc
 ln -s -f $DOTFILES/.tmux.conf        ~/.tmux.conf
 ln -s -f $DOTFILES/.vimrc            ~/.vimrc
 ln -s -f $DOTFILES/litecli/config    ~/.config/litecli/config
-ln -s -f $DOTFILES/ipython_config.py ~/.ipython/profile_default/ipython_config.py
