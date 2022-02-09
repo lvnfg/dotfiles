b153fa1a7d24db4f667d1e9225789bae7d86c32c
van <->
2022-02-09 16:39:51 +0000

    fix mssqlcli symlink

--- scripts/install-mssql-cli.sh
+++ scripts/install-mssql-cli.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 pip3 install mssql-cli
-ln -s -f $DOTFILES/mssqlcli/config ~/.config/mssqlcli/config
+mkdir -pv ~/.config/mssqlcli
+ln -s -f $DOTFILES/mssqlcli/config   ~/.config/mssqlcli/config
