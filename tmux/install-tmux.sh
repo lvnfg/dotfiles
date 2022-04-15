537e95d3430b67db24d5812689db1894ffc977ec
van <->
2022-04-15 08:10:18 +0000

    move tmux install to its own dir

---
+++ tmux/install-tmux.sh
@@ -0,0 +1,12 @@
+# The needed version is 3.2 for floating window.
+# Current tmux version in Debian 11 is 3.1.c.
+
+path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+ln -s -f "$path/.tmux.conf" ~/.tmux.conf
+
+# Use this when the apt package is 3.2
+# sudo apt install tmux -y
+
+# Install script for tmux
+
