f5b8da9cd612ea963805309ee601054edf5e76f3
van <->
2022-04-16 09:53:25 +0000

    add ranger install scripts

--- ranger/install-ranger.sh
+++ ranger/install-ranger.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
 
+path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Symlink ranger config
 mkdir ~/.config/ranger
-ln -s -f $DOTFILES/ranger/rc.conf	    ~/.config/ranger/rc.conf
-ln -s -f $DOTFILES/ranger/scope.sh	    ~/.config/ranger/scope.sh
+ln -s -f $path/rc.conf	    ~/.config/ranger/rc.conf
+ln -s -f $path/scope.sh	    ~/.config/ranger/scope.sh
 # Make scope.sh executable for ranger
-chmod +x $DOTFILES/ranger/scope.sh
+chmod +x $path/ranger/scope.sh
 sudo apt install ranger -y
 
 # Install devicons
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
-
-# ---------------------------------------------
-# This script is only needed because rnvimr plugin requires Ranger 1.9.3
-# above, while only 1.9.2 is available for Debian stable as of 2021-08-15.
-# Ranger built from source has permission error when calling preview script,
-# so preview will not work.
-#
-# Install using pip3 whenever a newer version is available.
-
-# cd ~
-# git clone https://github.com/ranger/ranger ~/.ranger
-# cd ~/.ranger
-# sudo make install
-
-# If Ranger has previously been installed via pip, launching Ranger
-# may throw an errro /usr/bin/ranger: No such file or directory. This
-# is because the older paths are hashes by bash. Remove all hash entries
-# using the command below will fix the problem.
-
-# hash -r
