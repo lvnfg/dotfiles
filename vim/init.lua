e5e974fbf749b420fd36a1a1a3b5253f65625daa
van <->
2022-05-09 23:25:17 +0000

    fix lua config

--- vim/init.lua
+++ vim/init.lua
@@ -75,6 +75,7 @@ g.gitgutter_eager = 1
 table.insert(plugins, 'junegunn/vim-easy-align')
 map('x', 'ga', '<Plug>(EasyAlign)')
 map('n', 'ga', '<Plug>(EasyAlign)')
+g.easy_align_ignore_groups = '[]'     -- Align everything. C-g to cycle through options interactively.
 
 -- Colorschemes
 -- table.insert(plugins, 'tomasr/molokai')
@@ -119,29 +120,29 @@ vim.keymap.set('i', '<S-Tab>', function()
 end, {expr = true})
 
 -- Closing & saving buffers & windows
-map('n', '<M-q>', ':q<cr>') -- Close window
+map('n', '<M-q>', ':q<cr>')                 -- Close window
 map('n', '<M-w>', ':lua CloseBuffer()<cr>') -- Call function close buffer
-map('n', '<M-s>', ':w<cr>') -- Save file
-map('n', '<M-S>', ':lua SaveAs()<cr>') -- Call SaveAs
-map('n', '<M-r>', ':lua RefreshAll()<cr>') -- Call RefreshAll()
+map('n', '<M-s>', ':w<cr>')                 -- Save file
+map('n', '<M-S>', ':lua SaveAs()<cr>')      -- Call SaveAs
+map('n', '<M-r>', ':lua RefreshAll()<cr>')  -- Call RefreshAll()
 
 -- Fzf
-map('n', '<M-f>', ':Files<cr>') -- Invoke Fzf
+map('n', '<M-f>', ':Files<cr>')             -- Invoke Fzf
 -- Git
-map('n', '<M-g>', ':w<cr>') -- Fzf list git status for all files
-map('n', '<M-c>', ':w<cr>') -- Fzf list git commit for this buffer
-map('n', '<M-C>', ':w<cr>') -- Fzf list git commit for entire repo
+map('n', '<M-g>', ':GFiles?<cr>')           -- Fzf list git status for all files
+map('n', '<M-c>', ':BCommits<cr>')          -- Fzf list git commit for this buffer
+map('n', '<M-C>', ':Commits<cr>')           -- Fzf list git commit for entire repo
 -- Buffer management
-map('n', '<M-b>', ':Buffers<cr>') -- Fzf list all buffers
+map('n', '<M-b>', ':Buffers<cr>')           -- Fzf list all buffers
 
 -- Navigation
-map('n', '<C-h>', ':bprevious<cr>') -- Show previous buffer in current window
-map('n', '<C-l>', ':bnext<cr>') -- Show next buffer in current window
+map('n', '<C-h>', ':bprevious<cr>')         -- Show previous buffer in current window
+map('n', '<C-l>', ':bnext<cr>')             -- Show next buffer in current window
 -- Tmux integrated movement
-map('n', '<M-h>', ':TmuxNavigateLeft<cr>') -- Silent?
-map('n', '<M-j>', ':TmuxNavigateDown<cr>') -- Silent?
-map('n', '<M-k>', ':TmuxNavigateUp<cr>') -- Silent?
-map('n', '<M-l>', ':TmuxNavigateRight<cr>') -- Silent?
+map('n', '<M-h>', ':TmuxNavigateLeft<cr>')
+map('n', '<M-j>', ':TmuxNavigateDown<cr>')
+map('n', '<M-k>', ':TmuxNavigateUp<cr>')
+map('n', '<M-l>', ':TmuxNavigateRight<cr>')
 -- Split window
 map('n', '<M-H>', ':aboveleft vsplit<cr>')
 map('n', '<M-J>', ':split<cr>')
