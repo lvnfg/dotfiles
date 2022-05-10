7340b66e97be9b48d37da681bc4ab6efe131a4bb
van <->
2022-05-10 03:15:30 +0000

    fix ipython conflict with python 3.10.4

--- ipython/install-ipython.sh
+++ ipython/install-ipython.sh
@@ -6,6 +6,9 @@ pip3 install ipython
 ipython profile create
 ln -s -f $path/ipython_config.py ~/.ipython/profile_default/ipython_config.py
 
+# Fix error with python 3.10.4
+pip3 install prompt_toolkit==3.0.29
+
 # To enable vim mode
 # Then open ~/.ipython/profile_default/ipython_config.py and set
 #   c.TerminalInteractiveShell.editing_mode = 'vi'  <-- Set to vi
