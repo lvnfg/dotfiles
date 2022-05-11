24ab68e842dbac5e5bf70729d0f18c46565af21c
van <->
2022-05-11 03:04:09 +0000

    move bash setup to install-bash.sh

--- setup-debian.sh
+++ setup-debian.sh
@@ -3,14 +3,6 @@
 script_name=$0
 path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ln -s -f $path/bash/.bashrc       ~/.bashrc
-ln -s -f $path/bash/.inputrc      ~/.inputrc
-ln -s -f $path/bash/.bash_profile ~/.bash_profile
-# ln -s -f $path/bash/.bashrc     ~/.profile       # .profile is reserved for env var, do not use
-
-# ----------------------------------------------
-# PACKAGED UTILITIES
-# ----------------------------------------------
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y jq
 sudo apt install -y wget
@@ -20,6 +12,7 @@ sudo apt install -y fzf
 sudo apt install -y rsync
 
 bash "$path/top/install-top.sh"
+bash "$path/top/install-bash.sh"
 bash "$path/tmux/install-tmux.sh"
 bash "$path/setup/install-git.sh"
 bash "$path/setup/install-bat.sh"
