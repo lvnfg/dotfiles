3de47361731715074bb795c45d4cd3ba28ae95f1
van <->
2022-05-28 04:42:20 +0000

    wip: docker install steps

---
+++ docker/install-docker.sh
@@ -0,0 +1,5 @@
+# Install docker for Debian
+# https://docs.docker.com/engine/install/debian/
+
+# Install docker in WSL 2 without Docker Desktop
+# https://dev.to/bowmanjd/install-docker-on-windows-wsl-without-docker-desktop-34m9
