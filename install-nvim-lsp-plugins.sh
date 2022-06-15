e390546b570e7b88f3bcb23bc66e211fa1774752
van <->
2022-06-15 04:00:11 +0000

    fix coc-nvim install

--- install-nvim-lsp-plugins.sh
+++ install-nvim-lsp-plugins.sh
@@ -4,13 +4,16 @@ set -euo pipefail
 NAME="neovim lsp plugins"
 echo $NAME 🚸
 
-# Install core plugins
+# ---------------------------------------------
+# COC-NVIM
+# ---------------------------------------------
 path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 mkdir -p "$path/vim/plugins"    # in .gitignore so not guaranteed to exists
-cd "$path/vim/plugins"
-# coc-nvim. Requires nodejs and npm
+# Require nodejs and npm
 apt-get install nodejs -y
 apt-get install npm -y
-git clone https://github.com/neoclide/coc.nvim || true
+cd "$path/vim/plugins"
+git clone --branch release https://github.com/neoclide/coc.nvim.git --depth=1 || true
+nvim -c "helptags coc.nvim/doc/ | q"
 
 echo $NAME ✅
