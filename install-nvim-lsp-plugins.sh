5665ce0715d8adb74adbe1fe231d036eb489ea8e
van <->
2022-06-15 03:52:07 +0000

    fix nvim lsp install

--- install-nvim-lsp-plugins.sh
+++ install-nvim-lsp-plugins.sh
@@ -5,6 +5,8 @@ NAME="neovim lsp plugins"
 echo $NAME 🚸
 
 # Install core plugins
+path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+mkdir -p "$path/vim/plugins"    # in .gitignore so not guaranteed to exists
 cd "$path/vim/plugins"
 # coc-nvim. Requires nodejs and npm
 apt-get install nodejs -y
