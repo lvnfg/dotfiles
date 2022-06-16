3ac03c68d741e194e956cebbd7c62bd71d9d168f
van <->
2022-06-16 03:16:04 +0000

    set container hostname to dot

--- docker-start-dot.sh
+++ docker-start-dot.sh
@@ -2,5 +2,6 @@ NAME="dot"
 
 sudo docker start -i $NAME || sudo docker run \
     -it \
+    --hostname dot \
     -v "$SSH_AUTH_SOCK:$SSH_AUTH_SOCK" -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
     --name $NAME $NAME:latest
