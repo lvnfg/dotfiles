30869b8503507891eba9b187b936c4b9f43c0dd2
van <->
2022-06-17 09:41:47 +0000

    fix: add python3.10 fix for ipython and mssqlcli

--- install-ipython.sh
+++ install-ipython.sh
@@ -6,6 +6,13 @@ pip3 install ipython
 ipython profile create
 ln -s -f $path/ipython/ipython_config.py $HOME/.ipython/profile_default/ipython_config.py
 
+# ipython somehow doesn't work with python 3.10
+pyver="$(python --version | grep 3.10)"
+if [[ "$pyver" != "" ]]; then
+    echo "Fixing prompt-toolkit"
+    pip3 install prompt_toolkit==3.0.29     # Must be at this specific version
+fi
+
 # To enable vim mode
 # Then open $HOME/.ipython/profile_default/ipython_config.py and set
 #   c.TerminalInteractiveShell.editing_mode = 'vi'  <-- Set to vi
