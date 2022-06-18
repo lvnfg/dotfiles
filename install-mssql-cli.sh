2c6be20ea5ae50e8d4fdb2e509cde25d4512242a
van <->
2022-06-18 09:35:05 +0000

    fix: mssql-cli install

--- install-mssql-cli.sh
+++ install-mssql-cli.sh
@@ -2,12 +2,12 @@
 set -euox pipefail
 path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# pip3 install mssql-cli
+pip3 install mssql-cli
 mkdir -pv $HOME/.config/mssqlcli
 ln -s -f $path/mssqlcli/config   $HOME/.config/mssqlcli/config
 
 # Fix cannot import collections.Iterables in python 3.10 +
-pyver="$(python --version | grep 3.10)"
+pyver="$(python3 --version | grep 3.10)"
 if [[ "$pyver" != "" ]]; then
     echo "Fixing collections import"
     ln -s -f $path/mssqlcli/collections__init__.py  /usr/local/lib/python3.10/collections/__init__.py
