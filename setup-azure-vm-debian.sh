d0f739922406a5e8d2492f0ef076534888916306
van <->
2022-12-20 02:08:03 +0000

    simplify azure vm debian setup

--- setup-azure-vm-debian.sh
+++ setup-azure-vm-debian.sh
@@ -4,14 +4,11 @@ path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Install utilities
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y jq
 sudo apt install -y wget
 sudo apt install -y unzip
 sudo apt install -y curl
 sudo apt install -y fzf
-sudo apt install -y rsync
 sudo apt install -y ripgrep     # Required for fzf.vim search all files
-# sudo apt install -y build-essential
 
 # Use -H option to keep $HOME as user
 bash "$path/install-top.sh"
@@ -21,15 +18,3 @@ bash "$path/install-git.sh"
 bash "$path/install-bat.sh"
 bash "$path/install-ranger.sh"
 bash "$path/install-nvim.sh"
-
-#-------- # Install docker
-#-------- bash "$path/install-docker.sh"
-#-------- # Set docker data location to temporary drive
-#-------- sudo systemctl stop docker
-#-------- sudo systemctl stop docker.socket
-#-------- sudo systemctl stop containerd
-#-------- mkdir /mnt/docker
-#-------- sudo mv /var/lib/docker /mnt/docker
-#-------- sudo cp -f "$path/docker/daemon.json" /etc/docker
-#-------- sudo systemctl start docker
-#-------- sudo docker info -f '{{ .DockerRootDir}}'
