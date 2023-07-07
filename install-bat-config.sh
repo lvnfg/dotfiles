16adba51b84061d7e168ea00a440a2786da09649
Van <van@fagaceae.com>
2023-07-07 13:20:05 +0700

    Update and rename install-bat.sh to install-bat-config.sh

--- install-bat-config.sh
+++ install-bat-config.sh
@@ -2,6 +2,5 @@
 set -euox pipefail
 if [ "$EUID" -ne 0 ]; then issudo="sudo"; else issudo=""; fi
 
-$issudo apt-get install -y bat
 mkdir -p $HOME/.local/bin
 $issudo ln -s -f /usr/bin/batcat $HOME/.local/bin/bat
