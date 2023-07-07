008d0721f97fd668dbd08d0b3179ff74d61e7434
Van <van@fagaceae.com>
2023-07-07 13:32:45 +0700

    Update and rename install-mssql-cli.sh to install-mssql-cli-config.sh

--- install-mssql-cli-config.sh
+++ install-mssql-cli-config.sh
@@ -2,7 +2,6 @@
 set -euox pipefail
 path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-pip3 install mssql-cli
 mkdir -pv $HOME/.config/mssqlcli
 ln -s -f $path/mssqlcli/config   $HOME/.config/mssqlcli/config
 
