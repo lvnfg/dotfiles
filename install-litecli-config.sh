37c802488de233988b16cafe2ade6ddfa837d5eb
Van <van@fagaceae.com>
2023-07-07 13:28:45 +0700

    Update and rename install-litecli.sh to install-litecli-config.sh

--- install-litecli-config.sh
+++ install-litecli-config.sh
@@ -2,6 +2,5 @@
 set -euox pipefail
 path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-pip3 install litecli
 mkdir -p $HOME/.config/litecli
 ln -s -f $path/litecli/config $HOME/.config/litecli/config
