00d0cbcc49d3aef71850bad0b58e20a30e930a6d
Van <van@fagaceae.com>
2023-07-07 13:26:20 +0700

    Delete install-fzf.sh

--- install-fzf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euox pipefail
-
-rm -rf $HOME/.fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-$HOME/.fzf/install
-ln -s -f $HOME/.fzf/bin/fzf  /usr/local/bin/fzf
