e9107d50a5da11ada65609e44ab3e75b7584da4c
Van <van@fagaceae.com>
2023-07-07 12:50:29 +0700

    Update install-tmux.sh

--- install-tmux.sh
+++ install-tmux.sh
@@ -8,8 +8,13 @@ ln -s -f "$path/tmux/.tmux.conf" $HOME/.tmux.conf
 # Install script for tmux
 # The needed version is 3.2 for floating window.
 
-# For Debian 12
-$issudo apt install tmux -y
-
-# For Debian 11, needs backports kernel
-# $issudo apt-get install tmux/bullseye-backports -y
+# Check Debian version
+DEBIAN_VER="$(cat /etc/debian_version)"
+if [ "$DEBIAN_VER" = "12.0" ];
+then 
+  # For Debian 12, install directly from repo
+  $issudo apt install tmux -y
+else 
+  # For Debian 11, needs backports kernel
+  $issudo apt-get install tmux/bullseye-backports -y
+fi
