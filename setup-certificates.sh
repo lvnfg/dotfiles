9cb9d2490637720faa5245369eb3c5fa25e891d0
van <van>
2025-01-06 14:20:07 +0000

    update certificate example

--- setup-certificates.sh
+++ setup-certificates.sh
@@ -3,6 +3,6 @@ openssl req -x509 \
     -days 3650               `# valid for X days` \
     -nodes                   `# 'No DES' = don't protect private key with a password` \
     -keyout example.key      `# or .pem, private key file` \
-    -out example.crt \       `# certificate file` \
-    -subj "/CN=Example"      \
+    -out example.crt \       `# or .pem, certificate file` \
+    -subj "/CN=example.com"  \
     -addext "subjectAltName=DNS:example.com,DNS:*.example.com,IP:10.0.0.1"  # List of valid addresses for this certficate
