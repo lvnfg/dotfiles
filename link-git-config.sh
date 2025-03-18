79097a4be2b46cf997a968333ff1f7efcd0f1913
van <van>
2025-03-18 13:50:57 +0000

    fix git config

--- link-git-config.sh
+++ link-git-config.sh
@@ -2,9 +2,9 @@
 set -euox pipefail
 if [ "$EUID" -ne 0 ]; then issudo="sudo"; else issudo=""; fi
 
-$issudo git config --global core.editor     "nvim"
-$issudo git config --global user.name       "van"
-$issudo git config --global user.email      "van@van"
+git config --global core.editor     "nvim"
+git config --global user.name       "van"
+git config --global user.email      "van@van"
 # git config --global format.graph
 # gitFormatFull="%C(auto)%h %d%Creset %s - %Cgreen%ad%Creset %aN <%aE>"
 # gitFormatShort="%C(auto)%h %d%Creset %s>"
@@ -12,10 +12,10 @@ $issudo git config --global user.email      "van@van"
 # git config format.pretty format:"$gitFormatString"
 
 # Merge tool
-$issudo git config --global diff.tool "nvimdiff"
-$issudo git config --global difftool.prompt false
-$issudo git config --global difftool.nvimdiff.cmd "nvim -d \"\$LOCAL\" \"\$REMOTE\""
-$issudo git config --global alias.d difftool
+git config --global diff.tool "nvimdiff"
+git config --global difftool.prompt false
+git config --global difftool.nvimdiff.cmd "nvim -d \"\$LOCAL\" \"\$REMOTE\""
+git config --global alias.d difftool
 
 # Pull
-$issudo git config --global pull.ff only
+git config --global pull.ff only
